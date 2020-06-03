fc9ee06ffdcf836482a5384b6aa6eb5674444663
eDi <littlexi1987@gmail.com>
2020-06-03 16:55:19 +0800

    update

--- src/main/java/com/example/spring/event/DemoPublisher.java
+++ src/main/java/com/example/spring/event/DemoPublisher.java
@@ -1,14 +1,16 @@
 package com.example.spring.event;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DemoPublisher {
 
-    @Autowired
-    private ApplicationContext applicationContext;
+    private final ApplicationContext applicationContext;
+
+    public DemoPublisher(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
 
     public void publish(String message) {
         applicationContext.publishEvent(new DemoEvent(this, message));
